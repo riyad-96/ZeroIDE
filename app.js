10c760ca84996f869fa15037901da892e6686134
Riyad <onlygobs0@gmail.com>
2025-04-27 14:35:49 +0600

    profile info and picture setup done

--- app.js
+++ app.js
@@ -288,7 +288,105 @@ window.addEventListener('DOMContentLoaded', pageChangeUpdate);
 
 //! Profile page updation programs
 //profile picture updation
+const savedProfileImg = localStorage.getItem('profile-image');
+const allProfileImgDisplay = document.querySelectorAll(
+  '.all-profile-image-display'
+);
+const profileImgInput = document.getElementById('profile-img-input');
+const profileImg = document.querySelector('.p-image');
+const previewImgContainer = document.querySelector('.img-container');
+const imgBtnContainer = document.querySelector('.img-buttons');
 
+profileImg.addEventListener('click', () => {
+  profileImgModal.classList.toggle('appear');
+});
+document.querySelector('.change-img-btn').addEventListener('click', () => {
+  profileImgInput.click();
+});
+// select profile picture
+profileImgInput.addEventListener('change', (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  reader.addEventListener('load', () => {
+    const imgFile = reader.result;
+    previewImgContainer.innerHTML = '';
+    const image = document.createElement('img');
+    image.src = imgFile;
+
+    const button = document.createElement('button');
+    button.classList.add('save-img-btn');
+    button.textContent = 'Save';
+
+    const oldSaveBtn = imgBtnContainer.querySelector('.save-img-btn');
+    if (oldSaveBtn) {
+      oldSaveBtn.remove();
+    }
+
+    imgBtnContainer.appendChild(button);
+    previewImgContainer.appendChild(image);
+    saveNewProfileImage(button, imgFile);
+  });
+});
+
+function saveNewProfileImage(saveBtn, img) {
+  saveBtn.addEventListener('click', () => {
+    localStorage.removeItem('profile-image');
+    localStorage.setItem('profile-image', img);
+    saveBtn.remove();
+    setTimeout(() => {
+      location.reload();
+    }, 300)
+  });
+}
+
+// load profile image in all field
+window.addEventListener('DOMContentLoaded', () => {
+  if (savedProfileImg) {
+    allProfileImgDisplay.forEach((container) => {
+      container.innerHTML = '';
+      const img = document.createElement('img');
+      img.src = savedProfileImg;
+      container.appendChild(img);
+    });
+  } else {
+    allProfileImgDisplay.forEach((container) => {
+      container.innerHTML = vectorProfileSvg();
+    });
+  }
+});
+
+//all image preview button action programs
+const downloadImgBtn = document.querySelector('.download-img-btn');
+
+downloadImgBtn.addEventListener('click', () => {
+  const savedImg = localStorage.getItem('profile-image');
+
+  if (savedImg) {
+    const a = document.createElement('a');
+    a.href = savedImg;
+    a.download = 'profile-image.png';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+});
+
+function vectorProfileSvg() {
+  return `<svg fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 45.53 45.53" xml:space="preserve" stroke="#1111111">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <g>
+                <path
+                  d="M22.766,0.001C10.194,0.001,0,10.193,0,22.766s10.193,22.765,22.766,22.765c12.574,0,22.766-10.192,22.766-22.765 S35.34,0.001,22.766,0.001z M22.766,6.808c4.16,0,7.531,3.372,7.531,7.53c0,4.159-3.371,7.53-7.531,7.53 c-4.158,0-7.529-3.371-7.529-7.53C15.237,10.18,18.608,6.808,22.766,6.808z M22.761,39.579c-4.149,0-7.949-1.511-10.88-4.012 c-0.714-0.609-1.126-1.502-1.126-2.439c0-4.217,3.413-7.592,7.631-7.592h8.762c4.219,0,7.619,3.375,7.619,7.592 c0,0.938-0.41,1.829-1.125,2.438C30.712,38.068,26.911,39.579,22.761,39.579z">
+                </path>
+              </g>
+            </g>
+          </svg>`;
+}
 
 // Profile data form updation
 const profileEditForm = document.querySelector('.profile-edit-form');
