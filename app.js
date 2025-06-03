4b4a118898d1d951fc0a001a94fd5e1f17039c7a
Riyad Hasan <onlygobs0@gmail.com>
2025-06-03 23:12:45 +0600

    preview section is under construction

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const savedVersion = localStorage.getItem('version');
 // 2 Jun 2025
 function migrateTo_v0_0_1() {
   const settingsObj = JSON.parse(localStorage.getItem('settings'));
-  if(settingsObj){
+  if (settingsObj) {
     settingsObj.editor.expandPanel = 'on';
     localStorage.setItem('settings', JSON.stringify(settingsObj));
   }
@@ -1031,11 +1031,9 @@ document.querySelector('.social-links').addEventListener('click', (e) => {
     document.execCommand('copy');
     document.body.removeChild(tempInput);
 
-    // btn.innerHTML = `<i class="fa-solid fa-check"></i>`;
     btn.innerHTML = checkIcon();
     btn.dataset.copied = 'true';
     setTimeout(() => {
-      // btn.innerHTML = `<i class="fa-regular fa-clone"></i>`;
       btn.innerHTML = clipboardIcon();
       delete btn.dataset.copied;
     }, 1500);
@@ -1113,7 +1111,7 @@ function loadLastFourProject(projects) {
         </div>
         <span>${p.des}</span>
         <div>
-          <span>created on ${date}</span>
+          <span>${date}</span>
         </div>`;
       recentProjectsContainer.prepend(div);
     }
@@ -1149,7 +1147,7 @@ const savedSettings = JSON.parse(localStorage.getItem('settings')) || {
     fontLigatures: 'on',
     tabSize: '2',
     autoRun: 'afterDelay',
-    expandPanel:'on',
+    expandPanel: 'on',
     semicolon: 'on',
     quotation: 'double',
     printWidth: '80',
@@ -1333,7 +1331,9 @@ inputArr.forEach((input) => {
 });
 
 //! load projects in projects page
-
+function eyeSvg() {
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3ZM12.0003 19C16.2359 19 19.8603 16.052 20.7777 12C19.8603 7.94803 16.2359 5 12.0003 5C7.7646 5 4.14022 7.94803 3.22278 12C4.14022 16.052 7.7646 19 12.0003 19ZM12.0003 16.5C9.51498 16.5 7.50026 14.4853 7.50026 12C7.50026 9.51472 9.51498 7.5 12.0003 7.5C14.4855 7.5 16.5003 9.51472 16.5003 12C16.5003 14.4853 14.4855 16.5 12.0003 16.5ZM12.0003 14.5C13.381 14.5 14.5003 13.3807 14.5003 12C14.5003 10.6193 13.381 9.5 12.0003 9.5C10.6196 9.5 9.50026 10.6193 9.50026 12C9.50026 13.3807 10.6196 14.5 12.0003 14.5Z"></path></svg>`;
+}
 function createProjectPageProjects(arr) {
   arr.forEach((p) => {
     const div = document.createElement('div');
@@ -1349,9 +1349,10 @@ function createProjectPageProjects(arr) {
       <footer>
         <div>
           <button data-project-id="${p.id}" class="add-favorite-btn" title="click to add/remove favorite">${p.favorite ? heartFillSvg() : heartLineSvg()}</button>
-          <button class="open-editor-btn" data-project-id="${p.id}">${openCodeSvg()}<span>Editor</span></button>
+          <button class="open-editor-btn" data-project-id="${p.id}" title="Open code editor">${openCodeSvg()}<span>Editor</span></button>
+          <button class="preview-editor-btn" data-project-id="${p.id}" title="Preview">${eyeSvg()}</button>
         </div>
-        <span>created on ${getDateFunc(p.date)}</span>
+        <span>${getDateFunc(p.date)}</span>
       </footer>
     `;
     projectPageProjectContainer.prepend(div);
@@ -1416,9 +1417,15 @@ projectPageProjectContainer.addEventListener('click', (e) => {
     }
   }
   // open editor btn
-  const openEditorBtn = e.target.closest('.open-editor-btn')
-  if(openEditorBtn) {
-    window.open(`./editor/user.html#${openEditorBtn.dataset.projectId}`)
+  const openEditorBtn = e.target.closest('.open-editor-btn');
+  if (openEditorBtn) {
+    window.open(`./editor/user.html#${openEditorBtn.dataset.projectId}`);
+  }
+  // open preview modal
+  const previewBtn = e.target.closest('.preview-editor-btn');
+  if (previewBtn) {
+    const previewModal = document.querySelector('.display-preview-modal');
+    previewModal.classList.add('show')
   }
 });
 
